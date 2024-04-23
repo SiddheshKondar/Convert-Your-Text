513396930dbe918109103d0791c8accb68f9d743
SiddheshKondar <siddheshkondar20@gmail.com>
2024-04-23 18:05:59 +0530

    Initial commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function (props) {
+  return (
+    <>
+<nav className={`navbar navbar-expand-lg navbar-${props.mode==="light"?"secondary":"dark"} bg-${props.mode==="light"?"secondary":"dark"}`}>
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">{props.title}</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" to="/home">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/about">About </Link>
+        </li>
+        
+      </ul>
+      <div className={`form-check form-switch text-${props.mode === "light"? "dark":"light"}`}>
+  <input className="form-check-input" onClick={props.togglemode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+  <label className="form-check-label" htmlFor="flexSwitchCheckDefault">{props.switchbtn} </label>
+</div>
+
+
+
+    </div>
+  </div>
+</nav>
+
+
+      
+    </>
+  );
+}
---
+++ src/Components/Textform.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+
+export default function Textform(props) {
+const handleupclick=()=>{
+  console.log("buttonwasclicked");
+  let newText= text.toUpperCase();
+  settext(newText);
+  props.showalert("Converted to Uppercase !", "success")
+}
+  const handlelowclick=()=>{
+    console.log("buttonwasclicked");
+    let newText= text.toLowerCase();
+    settext(newText);
+    props.showalert("Converted to Lowercase !", "success")
+
+}
+const handleonchange=(event)=>{
+  settext(event.target.value)
+
+}
+const cleartext=(event)=>{
+  let newText= "";
+  settext(newText);
+  props.showalert("Type your text to continue", "success")
+}
+
+
+
+const converttojsx=()=>{
+    let newtext= function htmlToJSX(html) {
+      html = html.replace(/<(\w+)>/g, "<$1 />");
+    
+      html = html.replace(/class=/g, "className=");
+  
+      html = html.replace(/for=/g, "htmlFor=");
+    
+      html = html.replace(/style=/g, "style={{");
+      html = html.replace(/;/g, "}}");
+    
+      return html;
+    }
+    settext(newtext);
+    }
+
+  const copytext=()=> {
+      var copyText = document.getElementById("myBox");
+      copyText.select();
+      navigator.clipboard.writeText(copyText.value);
+      props.showalert("Copied to clipboard", "success")
+    }
+
+  let [text, settext]= useState('');
+  return (
+    <div>
+        <div className="container">
+          <h1 className="my-2">{props.heading}</h1>
+          <textarea className="form-control my-3" onChange={handleonchange} value={text}  style={{backgroundColor: props.mode=== "dark"?"#919096":"white"}} id="myBox" rows="8" defaultValue={""} />
+            <button className="btn btn-primary mx-1" onClick={handleupclick}>Convert to uppercase </button>
+            <button className="btn btn-primary" onClick={handlelowclick}>Convert to lowercase </button>
+            <button className="btn btn-primary mx-1" onClick={cleartext}>Clear Text</button>
+            <button className="btn btn-primary " onClick={copytext}>Copy Text</button>
+            <button className="btn btn-primary mx-1" onClick={converttojsx}>Convert HTML to JSX</button>
+        </div>
+        <div className="container my-3"> 
+        <h1>Your text summary</h1>
+        <p>{text.split(" ").length}words and {text.length} characters</p>
+        <p>{0.008*text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text.length>0?text:"Enter text to preview"}</p>
+        
+        </div>
+      </div>
+  );
+}
